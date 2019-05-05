6ad5d2ad6d1fd49a468d2afdc8ca6e53e9a33029
nitinks <nitinksharma.viet@gmail.com>
2019-05-05 22:49:30 +0530

    Generic wait for function

--- WaitForState.py
+++ WaitForState.py
@@ -1,40 +1,60 @@
 # Wait until a state is reached.
 
-def WaitForVolumeConversion(self, timeout=1200, retrialPeriod=60):
+import time
+
+def WaitForVolumeConversion(func, desiredState, unexpectedState, timeout=1200, retrialPeriod=60, \
+                            retrialCount=20, raiseOnError=False, description=""):
     """
     Wait for Volume conversion task status to complete of fail.
+    :param func: function to be called for getting required state.
+    :param desiredState: Desired state to be reached within time.
+    :param unexpectedState: Unexpected state.
     :param timeout: wait time.
     :param retrialPeriod: probe time.
+    :param retrialCount: number of times retrial is wanted.
+    :param raiseOnError: Raise in case function raises error.
     :return: in case of success, raise on error.
     """
     startTime = time.time()
     remainingTime = timeout
+    
+    retrialRemain = retrialCount
 
-    while remainingTime > 0:
+    while remainingTime > 0 and retrialRemain > 0:
         currentTime = time.time()
         remainingTime = timeout - (currentTime - startTime)
 
-        # Wait for volume conversion task to complete.
-        taskState = self.GetVolumeTaskStatus()
+        # Wait for expected state to be reached.
+        try:
+            taskState = func(*args, **kwargs)
+        expect:
+            if raiseOnError:
+                raise Exception("Internal error while calling function")
+        
+        retrialRemain -= 1
 
-        # Example return values.
-        # 'State': 'active'|'cancelling'|'cancelled'|'completed',
-
-        if taskState == 'completed':
-            self.logger.info("Volume Conversion {} complete in {}s". \
-                             format(self.volumeTaskID, remainingTime))
+        if taskState == desiredState:
+            logger.info("Expected state reached {} complete in {}s". \
+                             format(desiredState, remainingTime))
             return
 
         # Raise in case exception occurred in pending operation.
-        if taskState == 'cancelling' or taskState == 'cancelled':
-            self.logger.error("Volume Conversion task abandoned with state{}". \
+        if taskState == unexpectedState:
+            logger.error("Waiting task abandoned with state{}". \
                               format(taskState))
-            raise Exception("Volume Conversion task abandoned with state{}" \
+            raise Exception("Waiting task abandoned with state{}" \
                             .format(taskState))
 
         time.sleep(retrialPeriod)
 
-    self.logger.debug("Timed out {}s waiting for Volume conversion Operation {}". \
-                      format(timeout, self.volumeTaskID))
-    raise Exception("Timed out waiting for Volume conversion Operation {}" \
-                    .format(self.volumeTaskID))
+    if remainingTime > 0:
+        logger.debug("Timed out {}s waiting for Operation {}". \
+                          format(timeout, desiredState))
+        raise Exception("Timed out waiting for  Operation {}" \
+                        .format(desiredState))
+        
+    if retrialRemain > 0
+        logger.debug("Retrial count hit while waiting for operation {}". \
+                          format(desiredState))
+        raise Exception ("Retrial count hit while waiting for operation {}". \
+                          format(desiredState))
